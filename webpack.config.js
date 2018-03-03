380aafa411bd8614ddac5e489f1be61d2b47e2e4
shogunsea <shogunsea08@gmail.com>
2018-03-03 14:58:32 -0800

    add webpack plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+    },
+  }),
+  new OptimizeCssAssetsPlugin(),
+];
+
+if (isProduction) {
+  const uglifyJS = new UglifyJSPlugin({
+    sourceMap: false,
+    mangle: false,
+  });
+  plugins.push(uglifyJS);
+}
 
 module.exports = {
   entry: './src/frontend/main.js',
@@ -6,6 +27,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, './public/playground/search_auto_complete')
   },
+  plugins: plugins,
   module: {
     rules: [
       {
